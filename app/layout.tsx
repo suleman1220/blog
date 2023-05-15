c88df5c4cee4fcaccf8619a7d3c0e381462ff5db
Suleman Tariq <suleman1220@gmail.com>
2023-05-15 06:46:37 +0500

    add google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import "@/styles/tailwind.css";
 import { Providers } from "./providers";
 import { cx } from "@/utils/all";
+import Script from "next/script";
 import { Inter, Lora } from "next/font/google";
 
 const inter = Inter({
@@ -23,7 +24,22 @@ export default function RootLayout({
       lang="en"
       suppressHydrationWarning
       className={cx(inter.variable, lora.variable)}>
-      <body className="antialiased text-gray-800 dark:bg-black dark:text-gray-400">
+      {/* Google Analytics */}
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-J7YTD5TR78"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+        
+          gtag('config', 'G-J7YTD5TR78');
+        `}
+      </Script>
+      {/* Google Analytics */}
+      <body className="text-gray-800 antialiased dark:bg-black dark:text-gray-400">
         <Providers>{children}</Providers>
       </body>
     </html>
