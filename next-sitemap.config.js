95273aa649a455260df1961afa0b6f28bf339170
Suleman Tariq <suleman1220@gmail.com>
2023-05-15 06:31:23 +0500

    update sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl:
-    process.env.SITE_URL || "https://stablo-pro.web3templates.com",
+  siteUrl: process.env.SITE_URL || "https://infobloginsider.com",
   generateRobotsTxt: true // (optional)
   // ...other options
 };
