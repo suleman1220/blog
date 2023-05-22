22bf524c9bf56f579b402cbfa7bea0b755b1c368
Suleman Tariq <suleman1220@gmail.com>
2023-05-22 16:00:21 +0500

    update sitemap url

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: process.env.SITE_URL || "https://infobloginsider.com",
+  siteUrl: process.env.SITE_URL || "https://www.infobloginsider.com",
   generateRobotsTxt: true // (optional)
   // ...other options
 };
